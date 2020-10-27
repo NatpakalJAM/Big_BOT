954322a7d87f9f5b0674503f22bbe1e73cda0509
NatpakalJAM <jam13432@gmail.com>
2020-10-27 14:30:32 +0700

    handler message list from JSON file

--- handler/message.go
+++ handler/message.go
@@ -1,6 +1,25 @@
 package handler
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"regexp"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+// message -> struct for message list
+type message struct {
+	Prefix   string `json:"prefix"`
+	Response struct {
+		Type string   `json:"type"`
+		List []string `json:"list"`
+	} `json:"response"`
+}
 
 // MessageCreate -> This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the authenticated bot has access to.
@@ -12,11 +31,44 @@ func MessageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 		return
 	}
 
-	if m.Content == "คะเมียวตําปรู๊ช" {
-		s.ChannelMessageSend(m.ChannelID, "คะเมียวตําปร๊าส")
-	}
+	// get message list from file
+	msgList := readMessageList()
 
-	if m.Content == "สาวๆเดินผ่านมา" {
-		s.ChannelMessageSend(m.ChannelID, "อะชิชิป่ะเห้ย์~")
+	// loop list message
+	for _, msg := range msgList {
+		// check regex match message string
+		match, _ := regexp.MatchString(msg.Prefix, m.Content)
+		if match {
+			// check message response type (accept: `normal` | `random`)
+			switch msg.Response.Type {
+			case "normal":
+				for _, res := range msg.Response.List {
+					s.ChannelMessageSend(m.ChannelID, res)
+				}
+			case "random":
+				rand.Seed(time.Now().UnixNano())
+				ranNum := rand.Intn(len(msg.Response.List))
+				s.ChannelMessageSend(m.ChannelID, msg.Response.List[ranNum])
+			}
+			// break loop ...
+			break
+		}
 	}
+}
+
+func readMessageList() (messageList []message) {
+	fileName := "./message/message.json"
+	f, err := os.Open(fileName)
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	byteValue, _ := ioutil.ReadAll(f)
+	json.Unmarshal(byteValue, &messageList)
+	f.Close()
+	if err != nil {
+		fmt.Println("error creating Discord session,", err)
+		return
+	}
+	return messageList
 }
